5ca9ba30e3cfc52e692f9163fbce6454ae9379da
sebagruia <sebastiangruia@gmail.com>
2019-08-30 14:07:03 +0200

    First Commit

---
+++ Js/app.js
@@ -0,0 +1,22 @@
+const input1 = document.getElementById('color1');
+const input2 = document.getElementById('color2');
+const h3 = document.getElementById('css');
+const linearGradingTextInput = document.getElementById('linear-gradient-content');
+const body = document.getElementById('gradient');
+const copyButton = document.getElementById('copy-button');
+
+
+const setGradient = () => {
+    body.setAttribute('style', `background: linear-gradient(to right, ${input1.value} , ${input2.value})`);
+    linearGradingTextInput.value = `linear-gradient(to-right, ${input1.value}, ${input2.value})`;
+}
+
+const copyLinearGradingHtml = ()=>{
+    linearGradingTextInput.select();
+    document.execCommand('copy');
+
+}
+
+input1.addEventListener('input', setGradient);
+input2.addEventListener('input', setGradient);
+copyButton.addEventListener('click', copyLinearGradingHtml);
---
+++ README.md
@@ -0,0 +1,30 @@
+# BACKGROUND GENERATOR
+
+## Table of Contents
+
+- [Project Info](#project_info)
+- [How In Works](#project_status)
+- [Web link](#web_link)
+
+
+## Project info
+
+* This a linear gradient generator app. It generates a color transition between two colors.
+
+## How it Works
+
+* You have to select two colors using the input color generators on the page, after this step 
+the linear-gradient line is outputed and you can copy it by using the "Copy Text" button.
+
+
+## Web Link
+
+[Link to the  GitHub Page](https://sebagruia.github.io/ToDo-List/)
+
+
+
+
+
+
+
+
